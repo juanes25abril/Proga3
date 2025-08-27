34abd7ccfaf369a173b84fcfdb57b05c688949a1
JuanFlaxan <148009664+juanes25abril@users.noreply.github.com>
2025-08-27 17:00:01 +0000

    update

--- Clase4/EjecutarLibro.java
+++ Clase4/EjecutarLibro.java
@@ -4,7 +4,7 @@ public class EjecutarLibro {
         
         Biblioteca Lib1 = new Biblioteca("Cien años de soledad", "Gabriel García Márquez", 01, 59900);
         Biblioteca Lib2 = new Biblioteca("1984", "George Orwell", 02,50000);
-        Biblioteca Lib3 = new Biblioteca(null, null, 0, 0)
+        Biblioteca Lib3 = new Biblioteca("null", null, 0, 0)
 
     }
 
