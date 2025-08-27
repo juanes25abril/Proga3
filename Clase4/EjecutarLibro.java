cd5945e5bbe085d0f3c01542e89db5d4d4a3cd52
JuanFlaxan <148009664+juanes25abril@users.noreply.github.com>
2025-08-27 17:25:32 +0000

    update

--- Clase4/EjecutarLibro.java
+++ Clase4/EjecutarLibro.java
@@ -4,9 +4,14 @@ public class EjecutarLibro {
 
         Biblioteca[] Bibliotecas = new Biblioteca[5];
         
-       Bibliotecas[0] = new Biblioteca(1, null, 0, null);
-       Bibliotecas[1] = new Biblioteca(1, null, 0, null);
-       Bibliotecas[2] = new Biblioteca(1, null, 0, null);
+       Bibliotecas[0] = new Biblioteca(1, null, 17800, null);
+       Bibliotecas[1] = new Biblioteca(2, null, 59000, null);
+       Bibliotecas[2] = new Biblioteca(3, null, 47800, null);
+       Bibliotecas[3] = new Biblioteca(4, null, 67900, null);
+       Bibliotecas[4] = new Biblioteca(5, null, 43900, null);
+
+       System.out.println("El Costo Total de los libros es " + Bibliotecas[3].CalcularPrecios(Bibliotecas) + " COP");
+
     }
 
     
