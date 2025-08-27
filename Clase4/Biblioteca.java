be677f3267589947447b9bc513fac66faa003bef
JuanFlaxan <148009664+juanes25abril@users.noreply.github.com>
2025-08-27 16:55:48 +0000

    update

--- Clase4/Biblioteca.java
+++ Clase4/Biblioteca.java
@@ -2,9 +2,56 @@ public class Biblioteca {
 
     private String Titulo;
     private String Autor;
+    private int id;
     private int Precio;
 
-    public
+    public Biblioteca(String titulo, String autor, int id, int precio) {
+        Titulo = titulo;
+        Autor = autor;
+        this.id = id;
+        Precio = precio;
+    }
+
+    public String getTitulo() {
+        return Titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        Titulo = titulo;
+    }
+
+    public String getAutor() {
+        return Autor;
+    }
+
+    public void setAutor(String autor) {
+        Autor = autor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPrecio() {
+        return Precio;
+    }
+
+    public void setPrecio(int precio) {
+        Precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Biblioteca [Titulo=" + Titulo + ", Autor=" + Autor + ", id=" + id + ", Precio=" + Precio + "]";
+    }
+
+    
+
+    
 
     
 }
---
+++ Clase4/EjecutarLibro.java
@@ -0,0 +1,12 @@
+public class EjecutarLibro {
+
+    public static void main(String[] args) {
+        
+        Biblioteca Lib1 = new Biblioteca("Cien años de soledad", "Gabriel García Márquez", 01, 59900);
+        Biblioteca Lib2 = new Biblioteca("1984", "George Orwell", 02,50000);
+        Biblioteca Lib3 = new Biblioteca(null, null, 0, 0)
+
+    }
+
+    
+} 
