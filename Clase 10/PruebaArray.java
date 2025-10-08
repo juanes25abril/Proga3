697ec9b4a6b272714e1e8605bef8a4660155dfe3
JuanFlaxan <148009664+juanes25abril@users.noreply.github.com>
2025-10-08 16:49:41 +0000

    Decay

---
+++ Clase 10/PruebaArray.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class PruebaArray {
+
+    public static void main(String[] args) {
+        ArrayList<String> nombres = new ArrayList<>();
+        nombres.add("Shirose");
+        nombres.add("Aila");
+        nombres.add("Yosef");
+        System.out.println(nombres);
+        System.out.println(nombres.size());
+
+    }
+
+    
+}
